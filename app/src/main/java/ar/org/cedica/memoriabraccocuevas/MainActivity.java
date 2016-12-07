9164e965a49554618ad97164f3d7db521e2a63e6
CEBracco <bracco66.cb@gmail.com>
2016-12-07 16:10:16 -0300

    settings menu

--- app/src/main/java/ar/org/cedica/memoriabraccocuevas/MainActivity.java
+++ app/src/main/java/ar/org/cedica/memoriabraccocuevas/MainActivity.java
@@ -1,5 +1,6 @@
 package ar.org.cedica.memoriabraccocuevas;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -37,7 +38,8 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            Snackbar.make(item.getActionView(),"holoo", Snackbar.LENGTH_LONG).show();
+            Intent intent= new Intent(this, SettingsActivity.class);
+            startActivity(intent);
             return true;
         }
 
