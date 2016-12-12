e762743b4cc5d5bad935ff0c1ed11297f026b5bd
lucascuevas <lucascuevas@live.com.ar>
2016-12-12 17:39:46 -0300

    vista imagen

--- app/src/main/java/ar/org/cedica/memoriabraccocuevas/MainActivity.java
+++ app/src/main/java/ar/org/cedica/memoriabraccocuevas/MainActivity.java
@@ -6,9 +6,17 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Layout;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,14 +26,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        ImageView img =(ImageView)findViewById(R.id.imageView);
+        ImageView img2 =(ImageView)findViewById(R.id.imageView2);
+        ImageView img3 =(ImageView)findViewById(R.id.imageView3);
+        ImageView img4 =(ImageView)findViewById(R.id.imageView4);
 
+        img.setBackgroundResource(R.drawable.casco);
+        img2.setBackgroundResource(R.drawable.casco);
+        img3.setBackgroundResource(R.drawable.casco);
+        img4.setBackgroundResource(R.drawable.casco);
 
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
+
         getMenuInflater().inflate(R.menu.menu_main, menu);
+       /* List<String> listita = new ArrayList<String>();
+        listita.add("holaa");
+        listita.add("El Hombre rata");
+        listita.add("holaa");
+        listita.add("El Hombre rata");
+        ListView l = (ListView) findViewById(R.id.listaImg);
+        ArrayAdapter<String> ada = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1,listita);
+        l.setAdapter(ada);
+        */
         return true;
     }
 
